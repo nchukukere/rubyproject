ff41347847eb4522160632c93309ab8996c51e8b
Nkemdi Chukukere <kendo37@gmail.com>
2017-12-22 19:32:54 -0500

    Cleaned up code

--- aws-test.rb
+++ aws-test.rb
@@ -1,28 +1,22 @@
 require 'aws-sdk'
 require 'rubygems'
 Aws.config.update({
-region: 'us-east-1',
-credentials: Aws::Credentials.new('AWSAccessKeyid', 'AWSSecretKey')
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new('AWSAccessKeyid', 'AWSSecretKey')
 })
 ec2 = Aws::EC2::Client.new
 puts ec2
-eresp= ec2.describe_instances
-puts eresp
-#puts eresp.reservations
-#eresp.reservations.first
-#eresp.reservations[0]
-#puts eresp[:reservations][0][:reservation_id]
-#puts eresp[:reservations][0][:instances][0][:tags][0][:value]
+eresp = ec2.describe_instances
+puts eresp# puts eresp.reservations# eresp.reservations.first# eresp.reservations[0]# puts eresp[: reservations][0][: reservation_id]# puts eresp[: reservations][0][: instances][0][: tags][0][: value]
 
-
-eresp[:reservations].each do |reservation|
-reservation[:instances].each do |instance|
-instance[:tags].each do |tag|
-puts tag[:value]
-end
-puts instance[:state][:name]
-puts instance[:instance_id]
-puts instance[:private_ip_address]
-puts instance[:instance_type]
+eresp[: reservations].each do |reservation |
+    reservation[: instances].each do |instance |
+      instance[: tags].each do |tag |
+        puts tag[: value]
+      end
+    puts instance[: state][: name]
+  puts instance[: instance_id]
+puts instance[: private_ip_address]
+puts instance[: instance_type]
 end
 end
--- findword.rb
+++ findword.rb
@@ -1,14 +1,13 @@
-f= File.open("task_list.txt")
+f = File.open("task_list.txt")
 linenumber = 3
 wordnumber = 4
-f.each_line do |line|
-	if $. == linenumber
-		values = line.split(' ')
-		if wordnumber > values.length
-			puts "String is small"
-		else
-			puts values[wordnumber-1]
-		end
-	end
+f.each_line do |line |
+  if $. == linenumber
+values = line.split(' ')
+if wordnumber > values.length
+puts "String is small"
+else
+  puts values[wordnumber - 1]
 end
-f.close
+end
+end
